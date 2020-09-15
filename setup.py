ba6761fc339c3131e95459a0d59bf024764fbb0e
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2020-09-15 14:29:20 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='VIA',
+    version='0.01', #15-Sep-2020
+    packages=['VIA',],
+    license='MIT',
+    author_email = 'shobana.venkat88@gmail.com',
+    url = 'https://github.com/ShobiStassen/VIA',
+    setup_requires = ['numpy','pybind11'],
+    install_requires=['pybind11','numpy','scipy','pandas','hnswlib','python-igraph','leidenalg==0.7.0','umap-learn','math','multiprocessing', 'sklearn', 'termcolor'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
