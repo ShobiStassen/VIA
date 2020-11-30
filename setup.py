c29485467c14db9cfee79c5c9cf20b91a8c13a14
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2020-11-30 11:04:47 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import sys
+from setuptools import setup
+from warnings import warn
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pyVIA',
+    version='0.1.3', #26-Nov-2020
+    packages=['pyVIA',],
+    license='MIT',
+    author_email = 'shobana.venkat88@gmail.com',
+    url = 'https://github.com/ShobiStassen/VIA',
+    setup_requires = ['numpy>=1.17','pybind11'],
+    install_requires=['pybind11','numpy>=1.17','scipy','pandas>=0.25','hnswlib','python-igraph','leidenalg==0.7.0', 'sklearn', 'termcolor','pygam'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
