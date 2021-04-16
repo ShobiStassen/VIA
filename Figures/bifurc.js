ab66040884f802b72fe98293b2e8619b832d3000
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2021-04-16 09:59:30 +0800

    Add files via upload

---
+++ Figures/bifurc.js
@@ -0,0 +1,48 @@
+var gData = {
+    "nodes": [
+        {
+          "id": "id1",
+          "name": "name1",
+          "val": 1
+        },
+        {
+          "id": "id2",
+          "name": "name2",
+          "val": 2
+        },
+        {
+          "id": "id3",
+          "name": "name3",
+          "val": 5
+        },
+        {
+          "id": "id4",
+          "name": "name4",
+          "val": 10
+        },
+    ],
+    "links": [
+        {
+            "source": "id1",
+            "target": "id2",
+            "distance":300,
+            "width": 1
+
+        },
+        {
+            "source": "id2",
+            "target": "id3",
+            "distance":10,
+            "width": 2
+
+        },
+        {
+            "source": "id2",
+            "target": "id4",
+            "distance":10,
+            "width": 2
+
+        },
+
+    ]
+}
