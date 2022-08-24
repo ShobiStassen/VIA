d2b632799c8dd3fcd4fc4875b72ed441aff44637
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2022-08-24 12:48:05 +0800

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..VIA'))
+#sys.path.insert(0, os.path.abspath('..VIA'))
+sys.path.insert(0, os.path.abspath('../..'))
 #root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 #sys.path.insert(0, root_dir)
 
