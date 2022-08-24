bfca17225a554ad9124dc11d4135b009913ff76a
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2022-08-24 12:20:48 +0800

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('/home/shobi/Trajectory/VIA'))
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-sys.path.insert(0, root_dir)
+sys.path.insert(0, os.path.abspath('..VIA'))
+#root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+#sys.path.insert(0, root_dir)
 
 
 # -- Project information -----------------------------------------------------
