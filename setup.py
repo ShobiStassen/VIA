5cb405cb7e1460dee28990e7d3d0c9b3567132aa
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2022-11-22 19:34:17 +0800

    v0.1.59

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='pyVIA',
-    version='0.1.58', #Nov-21-2022
+    version='0.1.59', #Nov-22-2022
     packages=['pyVIA',],
     license='MIT',
     author_email = 'shobana.venkat88@gmail.com',
