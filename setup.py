7adddc1cd4153ada357bd850b8c0189ac3c7d0a2
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2022-12-12 13:11:07 +0800

    v0.1.64
    allow user to pass predefined single-cell integer list of cluster labels

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='pyVIA',
-    version='0.1.63', #Dec-06-2022
+    version='0.1.64', #Dec-12-2022
     packages=['pyVIA',],
     license='MIT',
     author='Shobi Stassen'
