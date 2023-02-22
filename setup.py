5f9daf3442c9aca9d1f5da8f29b2a22284fafa62
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2023-02-22 18:16:50 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='pyVIA',
-    version='0.1.73', #Jan-06-2022
+    version='0.1.77', #Feb-22-2022
     packages=['pyVIA',],
     license='MIT',
     author_email = 'shobana.venkat88@gmail.com',
     url = 'https://github.com/ShobiStassen/VIA',
     setup_requires = ['numpy>=1.17','pybind11'],
-    install_requires=['pybind11','numpy>=1.17','scipy','pandas>=0.25','hnswlib','igraph','leidenalg>=0.7.0', 'scikit-learn', 'termcolor','pygam', 'matplotlib','scanpy','umap-learn','phate','datashader', 'scikit-image', 'pillow','wget','gdown','seaborn'],
+    install_requires=['pybind11','numpy>=1.17','scipy','pandas>=0.25','hnswlib','igraph','leidenalg>=0.7.0', 'scikit-learn', 'termcolor','pygam', 'matplotlib','scanpy','umap-learn>=0.5.0','phate','datashader', 'scikit-image', 'pillow','wget','gdown','seaborn'],
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
