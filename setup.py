d083dbecf08dfc980da89e49cfdca6eb605d32b3
Shobi Stassen <35950152+ShobiStassen@users.noreply.github.com>
2023-11-22 08:46:41 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='pyVIA',
-    version='0.1.94', ##October 2023 # May-2-2023
+    version='0.1.96', ##Nov 22, 2023
     packages=['pyVIA',],
     license='MIT',
     author_email = 'shobana.venkat88@gmail.com',
